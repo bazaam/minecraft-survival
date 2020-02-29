9fe27b18aeba0bb9288684bb3c1029b1af0012ee
bazaam <bazaam1@gmail.com>
2020-02-28 18:27:18 -0800

    Zombie spawning test

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,20 +1,34 @@
 package com.example.examplemod;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.event.entity.item.ItemEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickItem;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "testmod";
+    public static final String NAME = "Test Mod";
+    public static final String VERSION = "0.01";
 
     private static Logger logger;
+    
+    public static ToolMaterial testToolMaterial;
+    public static Item myItem;
+    public static Block myBlock;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
@@ -27,5 +41,20 @@ public class ExampleMod
     {
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+    
+    @Mod.EventBusSubscriber
+    public static class ZombieWandSpawnHandler {
+    	@SubscribeEvent
+    	public static void stickUsed(RightClickItem event) {
+    		if (event.getItemStack().getItem() == Items.STICK) {
+        		EntityPlayer player = (EntityPlayer)event.getEntity();
+        		EntityZombie zombie = new EntityZombie(player.world);
+        		zombie.setLocationAndAngles(player.posX - 10, player.posY, player.posZ, 0, 0);
+        		if (!player.world.isRemote) {
+        			player.world.spawnEntity(zombie);
+        		}
+    		}
+    	}
     }
 }
